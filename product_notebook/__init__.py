0531ab4274ecc4883cd7f90b366c81c7fa26a666
xbox89950-ux <xbox89950@gmail.com>
2026-08-15 05:14:26 +0100

    feat(ai1): initialize product notebook package

---
+++ product_notebook/__init__.py
@@ -0,0 +1,6 @@
+"""AI 1 Product Notebook package."""
+
+from product_notebook.models import ProductRecord
+from product_notebook.repository import ProductNotebook
+
+__all__ = ["ProductRecord", "ProductNotebook"]
